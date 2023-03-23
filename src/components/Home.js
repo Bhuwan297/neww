69447efc70cbcf6efc59b479552f80d8ba481fa1
CEPINNOCK <edwardfinancee@outlook.com>
2023-03-23 23:40:16 +0000

    navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import './css/home.css'
 const Home = () => {
   return (
     <div className="community">
+      HOME PAGE
 
     </div>
   );
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,38 +1,59 @@
 import { Outlet, Link } from "react-router-dom";
-import Footer from './Footer'
-import '../components/css/Layout.css'
-
+import Footer from './Footer';
+import '../components/css/Layout.css';
+import { useState } from 'react';
 
 const Layout = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <>
-    <nav id="navbar">
-   
-
-      
-        <ul className="navbar-links">
-   
-            <Link to="/" className="navabr-link">Home</Link>
-        
-       
-            <Link to="/login" className="navabar-link"> Login</Link>
-
-     
-            <Link to="/fresher" className="navabar-link"> FresherGuide</Link>
-       
-    
-            <Link to="/contact" className="navbar-link"> Contact</Link>
-         
-        </ul>
-   
+    <div>
+      <nav className="nav">
+        <div className="nav-main">
+          <div className="logo">FRESH<span>START</span></div>
+          <ul className="nav-links">
+            <li className="nav-link"><Link to="/">Home</Link></li>
+            <li className="nav-link"><Link to="/team">Our Team</Link></li>
+            <li className="nav-link dropdown">
+              <a href="" className="dropdown">Services <i className="bi bi-chevron-compact-down"></i></a>
+              <ul className="dropdown-list">
+                <li className="nav-link"><a href="">Service 1</a></li>
+                <li className="nav-link"><a href="">Service 2</a></li>
+              </ul>
+            </li>
+            <li className="nav-link"><Link to="/">Blog</Link></li>
+          </ul>
+        </div>
+        <div className="cta">
+          <button className="btn btn-secondary">Log In</button>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+        <div className="menu">
+          <button className="btn btn-primary menu" onClick={handleMenuClick}><i className="bi-list"></i></button>
+          {menuOpen && (
+            <ul className="nav-mobile">
+              <li className="nav-link"><Link to="/">Home</Link></li>
+              <li className="nav-link"><Link to="/">Our Team</Link></li>
+              <li className="nav-link dropdown">
+                <a href="" className="dropdown">Services <i className="bi bi-chevron-compact-down"></i></a>
+                <ul className="dropdown-list">
+                  <li className="nav-link"><a href="">Service 1</a></li>
+                  <li className="nav-link"><a href="">Service 2</a></li>
+                </ul>
+              </li>
+              <li className="nav-link"><Link to="/blog">Blog</Link></li>
+            </ul>
+          )}
+        </div>
       </nav>
-      <br></br>
-   
       <Outlet />
-
       <Footer />
-    </>
-  )
+    </div>
+  );
 };
 
 export default Layout;
