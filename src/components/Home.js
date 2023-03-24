ee256d98235d27760ca202b8e23ec7b981d9281c
CEPINNOCK <edwardfinancee@outlook.com>
2023-03-24 00:51:02 +0000

    landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import aboutus from '../images/aboutus.png'
-
+import heroimg from '../images/heroimg.png'
+import companies from '../images/jobspartners.png'
 import './css/home.css'
 
 
@@ -20,6 +21,7 @@ Relocating to the UK is a daunting process for international students.
  Click on the button below to get started
 </p>
 
+<img src={heroimg} className='heroimg' />
 
 
    </div>
@@ -48,6 +50,60 @@ At Freshstart, our goal is to help international
 </p>
 <img src={aboutus} className='aboutus' />
    </div>
+
+
+
+   <div className='FresherGuide'>
+
+    <h3>FRESHER GUIDE</h3>
+    <p>
+    Nisi fermentum elementum quam volutpat neque lectus est. 
+    In vel lacus tempor pretium. Quis id et interdum id volutpat
+     amet eget quis integer. Ut morbi non.
+
+    </p>
+   </div>
+
+<div className='ourservices'>
+  <h3>OUR SERVICES</h3>
+ 
+  <p>
+    
+  Here at the university of Wolverhampton we aim to give you 
+  the best experience of the city of Wolverhampton. Read more..
+  </p>
+
+<div className='services' id="blueback">
+  <h3>DISCOUNT GAME TICKETS</h3>
+  <p>Enjoy Live Matches in the Molienux Stadium with Discount Prizes</p>
+</div>
+<div className='services' id="yellowback">
+  <h3>UPCOMING EVNTS</h3>
+  <p>
+    Subscribe to our Newsletter for exciting UPCOMING evnts
+  </p>
+</div>
+
+<div className='services' id="blueback">
+  <h3>WEEKEND TOURS</h3>
+  <br></br>
+  <p>
+    Enjoy Tours Around The West Midlands
+  </p>
+</div>
+
+<div className='services' id="yellowback">
+  <h3>PART TIME JOBS</h3>
+  <p>
+    Make Money while you study.
+  </p>
+</div>
+<br></br>
+
+<a href='#' className='viewmore'> VIEW MORE SERVICES</a>
+
+
+</div>
 
     </div>
   );
